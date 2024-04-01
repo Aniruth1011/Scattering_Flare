dad66e9791164c7dbab5a6cb521fbeaaafcfbb01
Aniruth1011 <aniruths10@gmail.com>
2024-04-01 23:17:46 +0530

    added lens component. 1 error left

--- net/lens.py
+++ net/lens.py
@@ -1,59 +1,68 @@
-# import torch
-# import torch.nn as nn
-
-# class LensComponent(nn.Module):
-#     def __init__(self, resnet, transformer):
-#         super(LensComponent, self).__init__()
-#         self.resnet = resnet
-#         self.transformer = transformer
-#         self.flatten = nn.Flatten()
-#         self.tanh = nn.Tanh()
-
-#     def forward(self, x_resnet, x_transformer):
-#         features_resnet = self.resnet(x_resnet)
-#         flattened_features = self.flatten(features_resnet)
-#         tanh_output = self.tanh(flattened_features)
-#         # lens_output = torch.dot(tanh_output, x_transformer)
-        
-#         return tanh_output
-
-# resnet_model = ...  # model weights
-# transformer_model = 'ckpt/unet_model.pth'  # trans weights
-
-# # Initialize LensComponent
-# lens_component = LensComponent(resnet=resnet_model, transformer=transformer_model)
-
-# resnet_input = torch.randn(1, 512, 128, 128)
-# # resnet_input = (1, 128, 128, 128)
-# transformer_input = torch.randn(1, trans_size_podanum) 
-# output = lens_component(resnet_input, transformer_input)
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.models as models
 
-class LensComponent(nn.Module):
-    def __init__(self):
-        super(LensComponent, self).__init__()
-        # Load pre-trained ResNet
-        self.resnet = models.resnet18(pretrained=True)
-        # Disable gradient computation for ResNet
-        for param in self.resnet.parameters():
-            param.requires_grad = False
-        self.flatten = nn.Flatten()
+class ResidualBlock3D(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super(ResidualBlock3D, self).__init__()
+
+        self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size, stride, padding)
+        self.bn1 = nn.BatchNorm3d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size, 1, padding)
+        self.bn2 = nn.BatchNorm3d(out_channels)
+
+        if stride != 1 or in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv3d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.BatchNorm3d(out_channels)
+            )
+        else:
+            self.downsample = None
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class LensBlock(nn.Module):
+    def __init__(self , input_dim):
+        super(LensBlock, self).__init__()
+
+        self.conv1 = nn.Conv3d(256, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm3d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.residual_block1 = ResidualBlock3D(64, 64)
+        self.residual_block2 = ResidualBlock3D(64, 128)
+        self.conv2 = nn.Conv3d(128, input_dim, kernel_size=3, padding=1)
+        
         self.tanh = nn.Tanh()
 
-    def forward(self, x_transformer):
-        # Pass transformer output through ResNet
-        features_resnet = self.resnet(x_transformer)
-        flattened_features = self.flatten(features_resnet)
-        tanh_output = self.tanh(flattened_features)
-        return tanh_output
+    def forward(self, x):
 
-# Example usage
-# Assuming x_transformer is your input tensor from previous transformer layers
-x_transformer = torch.randn(1, 3, 224, 224)  # Example input tensor
-lens_component = LensComponent()
-output = lens_component(x_transformer)
-print(output.shape)  # Print the shape of the output tensor
+        x = self.conv1(x)
+
+        print("con1" , x.shape)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.residual_block1(x)
+        x = self.residual_block2(x)
+
+        x = self.conv2(x)
+        
+        x = self.tanh(x)
+
+        return x
